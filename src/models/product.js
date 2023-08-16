2c8f531bd0b8a6c9e1adf54d9b653311fc5f1cff
mansi-kathiriya <mansikathiriya08@gmail.com>
2023-08-16 09:33:23 +0530

    add file

---
+++ src/models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+    {
+        product_id: {
+            type: String,
+            trim: true,
+        },
+        product_name: {
+            type: String,
+            trim: true,
+        },
+        product_desc: {
+            type: String,
+            trim: true,
+        },
+        price: {
+            type: Number,
+            trim: true,
+        },
+        is_active: {
+            type: Boolean,
+            default: true,
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+const product = mongoose.model("product",productSchema);
+
+module.exports = product;
