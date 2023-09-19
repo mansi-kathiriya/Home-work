98ebcb6991f15833ba5637c7df407b87e50cc48c
mansi-kathiriya <mansikathiriya08@gmail.com>
2023-09-19 08:51:41 +0530

    # upload

---
+++ src/middlewares/upload.js
@@ -0,0 +1,27 @@
+const multer = require("multer");
+const fs = require("fs");
+const path = ("path");
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        if(file.fieldname == "image"){
+            fs.mkdirSync(path.join(__dirname, "../public/image"), {
+                recursive: true,
+            });
+            cb(null, path.join(__dirname, "../public/image"))
+        }
+    },
+    filename: function(req, file, cb) {
+        const ext = path.extname(file.originalname);
+        if(ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg"){
+            cb("Only .png , .jpg and .jpeg format are allowed!");
+        }
+        cb(null, new Date().getTime() + ext);
+    },
+});
+
+const upload = multer({
+    storage: storage,
+});
+
+module.exports = { upload };
